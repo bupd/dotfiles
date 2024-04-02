b8adaf5952e4703a9b56ea2a866f4d57d8721e14
bupd <bupdprasanth@gmail.com>
2024-04-02 10:02:27 +0530

    add neovim to the config
    Signed-off-by: bupd <bupdprasanth@gmail.com>

---
+++ .config/nvim/lua/custom/mappings.lua
@@ -0,0 +1,24 @@
+local M = {}
+M.general = {
+  n={
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+  }
+}
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line",
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start or continue the debugger",
+    },
+  },
+}
+return M
+
