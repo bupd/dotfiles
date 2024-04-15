9f991d6149787d2a226fb03ab831e4f1e5fd0963
bupd <bupdprasanth@gmail.com>
2024-04-15 18:46:57 +0530

    remove: nvim-dap plugin
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -4,25 +4,6 @@ local plugins = {
   {'christoomey/vim-tmux-navigator',
     lazy=false, },
   {
-    "rcarriga/nvim-dap-ui",
-    event = "VeryLazy",
-    dependencies = "mfussenegger/nvim-dap",
-    config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-      dapui.setup()
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end,
-  },
-  {
   "mbbill/undotree",
   dependencies = "nvim-lua/plenary.nvim",
   -- config = true,
@@ -45,34 +26,6 @@ local plugins = {
     },
     config=function ()
      require("nvim-ts-autotag").setup()
-    end
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    event = "VeryLazy",
-    dependencies = {
-    "williamboman/mason.nvim",
-    "mfussenegger/nvim-dap",
-    },
-      opts = {
-        handlers = {},
-    },
-  },
-  {
-    "mfussenegger/nvim-dap",
-    config = function (_, _)
-      require("core.utils").load_mappings("dap")
-    end
-  },
-  {
-    "mfussenegger/nvim-dap-python",
-    ft="python",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-    },
-    config = function (_, opts)
-      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-      require("dap-python").setup(path)
     end
   },
   {
