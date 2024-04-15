2596007581508175c53273dca27a9a66850e09a6
bupd <bupdprasanth@gmail.com>
2024-04-15 18:47:16 +0530

    update: null-ls to set only to prettier to format
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/custom/configs/null-ls.lua
+++ .config/nvim/lua/custom/configs/null-ls.lua
@@ -3,12 +3,7 @@ local null_ls = require("null-ls")
 
 local opts = {
   sources = {
-    null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.diagnostics.mypy,
-    null_ls.builtins.diagnostics.ruff,
-
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
