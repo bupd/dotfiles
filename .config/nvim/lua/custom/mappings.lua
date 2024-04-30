1131537ceb39db34f351eb09d7e6add94d5e2203
bupd <bupdprasanth@gmail.com>
2024-05-01 01:33:30 +0530

    remove mappings for tmux

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -4,7 +4,6 @@ M.general = {
     ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
     ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
     ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
-    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
   }
 }
 M.dap = {
