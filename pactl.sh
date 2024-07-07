b11b5d767ed02eee3176610343b75afd2697e477
bupd <bupdprasanth@gmail.com>
2024-07-07 22:24:58 +0530

    improve pactl with naming the null audio sinks
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- pactl.sh
+++ pactl.sh
@@ -11,8 +11,8 @@ echo "Sink number for $sink_name: $sink_number"
 
 # Load the module-null-sink and display the new sink ID
 echo -n "Loading module-null-sink... "
-null_sink_id=$(pactl load-module module-null-sink)
-null_sink_id2=$(pactl load-module module-null-sink)
+null_sink_id=$(pactl load-module module-null-sink sink_name=music sink_properties=device.description="music")
+null_sink_id2=$(pactl load-module module-null-sink sink_name=out sink_properties=device.description="out")
 
 # Get number of null Sink.
 null_sink_number=$(pactl list short sinks | grep module-null-sink.c | awk 'NR==1{print $1}')
@@ -24,7 +24,7 @@ echo -e "\nNull-Sink Number: ${null_sink_number2}"
 # Load the module-combine-sink with the desired slaves and display the new sink ID
 echo -n "Loading module-combine-sink with slaves $sink_number and $null_sink_number... "
 echo -n "Loading module-combine-sink with slaves $sink_number and $null_sink_number2... "
-combine_sink_id=$(pactl load-module module-combine-sink slaves=$sink_number,$null_sink_number)
+combine_sink_id=$(pactl load-module module-combine-sink sink_name=forOut slaves=$sink_number,$null_sink_number)
 combine_sink_id2=$(pactl load-module module-combine-sink slaves=$sink_number,$null_sink_number2)
 # echo "combined_Sink ID: $combine_sink_id"
 # echo "combined_Sink ID: $combine_sink_id2"
