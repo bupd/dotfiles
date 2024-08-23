68b8ccd52919f21bfe520d04788aa1482c12cd8d
bupd <bupdprasanth@gmail.com>
2024-08-23 13:22:26 +0530

    neovim remove redundant trailing spaces
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -3,5 +3,11 @@ vim.opt.relativenumber = true
 
 vim.cmd('set guicursor=n-v-c:block-Cursor-blinkwait1000-blinkon500-blinkoff300')
 
--- bupd 
+-- remove redundant whitespace
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]],
+})
+
+-- bupd
 
