dc1efb1b10eb1ac4135b0b7065c06b94be442c8a
bupd <bupdprasanth@gmail.com>
2024-09-08 23:12:19 +0530

    turn off swap file
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -3,6 +3,9 @@ vim.opt.relativenumber = true
 
 vim.cmd('set guicursor=n-v-c:block-Cursor-blinkwait1000-blinkon500-blinkoff300')
 
+-- turn off swap file
+vim.opt.swapfile = false
+
 -- remove redundant whitespace
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
