8c0f1ab8a27a04e4a227d30c3816e1a825f8eb78
bupd <bupdprasanth@gmail.com>
2024-09-11 19:59:03 +0530

    remove trailing whitespaces comment it out
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -12,18 +12,18 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   command = [[%s/\s\+$//e]],
 })
 
--- Auto-reindent and remove trailing whitespace on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function()
-    -- Remove trailing whitespace
-    vim.cmd([[%s/\s\+$//e]])
-    -- Save the current cursor position
-    local current_pos = vim.api.nvim_win_get_cursor(0)
-    -- Reindent the entire file
-    vim.cmd("normal! gg=G")
-    -- Restore the cursor position
-    vim.api.nvim_win_set_cursor(0, current_pos)
-  end,
-})
+-- -- Auto-reindent and remove trailing whitespace on save
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*",
+--   callback = function()
+--     -- Remove trailing whitespace
+--     vim.cmd([[%s/\s\+$//e]])
+--     -- Save the current cursor position
+--     local current_pos = vim.api.nvim_win_get_cursor(0)
+--     -- Reindent the entire file
+--     vim.cmd("normal! gg=G")
+--     -- Restore the cursor position
+--     vim.api.nvim_win_set_cursor(0, current_pos)
+--   end,
+-- })
 -- bupd
