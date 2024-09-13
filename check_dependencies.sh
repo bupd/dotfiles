3edf2c01c9caf63fde91440c7a265c90dad95e27
bupd <bupdprasanth@gmail.com>
2024-09-13 15:30:08 +0530

    add check dependency script
    Signed-off-by: bupd <bupdprasanth@gmail.com>

---
+++ check_dependencies.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# List of required programs
+required_programs=(
+    "stow"
+    "git"
+    "i3"
+    "i3status"
+    "nvim"
+    "picom"
+    "pactl"
+    "zsh"
+    "bash"
+)
+
+# Check for missing programs
+missing_programs=()
+for program in "${required_programs[@]}"; do
+    if ! command -v "$program" &> /dev/null; then
+        missing_programs+=("$program")
+    fi
+done
+
+# Report missing programs
+if [ ${#missing_programs[@]} -eq 0 ]; then
+    echo "All required programs are installed."
+else
+    echo "The following programs are missing:"
+    for program in "${missing_programs[@]}"; do
+        echo "- $program"
+    done
+    echo "Please install the missing programs and try again."
+fi
+
