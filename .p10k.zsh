a0b6d5246faecfde10ff66c12bbf6d07ee2b5a68
bupd <bupdprasanth@gmail.com>
2024-09-13 23:57:59 +0530

    update p10k
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .p10k.zsh
+++ .p10k.zsh
@@ -32,7 +32,7 @@
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
     os_icon                 # os identifier
-    # dir                     # current directory
+    dir                     # current directory
     vcs                     # git status
     # =========================[ Line #2 ]=========================
     newline                 # \n
