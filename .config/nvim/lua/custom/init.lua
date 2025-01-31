888474dbbf036ebc009875824e86fd5871fa12b4
bupd <bupdprasanth@gmail.com>
2025-02-01 05:21:41 +0530

    format init lua
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -1,15 +1,15 @@
-vim.opt.colorcolumn = '80';
+vim.opt.colorcolumn = "80"
 vim.opt.relativenumber = true
 
-vim.cmd('set guicursor=n-v-c:block-Cursor-blinkwait1000-blinkon500-blinkoff300')
+vim.cmd("set guicursor=n-v-c:block-Cursor-blinkwait1000-blinkon500-blinkoff300")
 
 -- turn off swap file
 vim.opt.swapfile = false
 
 -- remove redundant trailing whitespace
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*" },
-  command = [[%s/\s\+$//e]],
+	pattern = { "*" },
+	command = [[%s/\s\+$//e]],
 })
 
 -- jump to errors
@@ -18,33 +18,33 @@ vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
 
 -- Remove current item from quickfix list and adjust navigation
 function RemoveQFItem()
-  -- Get the current quickfix list and current line index
-  local qfall = vim.fn.getqflist()
-  local curqfidx = vim.fn.line('.') - 1  -- current quickfix index
+	-- Get the current quickfix list and current line index
+	local qfall = vim.fn.getqflist()
+	local curqfidx = vim.fn.line(".") - 1 -- current quickfix index
 
-  -- Remove the current item from the quickfix list
-  table.remove(qfall, curqfidx + 1)  -- Lua is 1-indexed, hence `+1`
+	-- Remove the current item from the quickfix list
+	table.remove(qfall, curqfidx + 1) -- Lua is 1-indexed, hence `+1`
 
-  -- Update the quickfix list
-  vim.fn.setqflist(qfall, 'r')
+	-- Update the quickfix list
+	vim.fn.setqflist(qfall, "r")
 
-  -- Move to the next item or the previous one after removing an item
-  if curqfidx >= #qfall then
-    vim.cmd('cprev')  -- If we're at the last item, go to the previous item
-  else
-    vim.cmd('cfirst')  -- Otherwise, go to the first item after removal
-  end
+	-- Move to the next item or the previous one after removing an item
+	if curqfidx >= #qfall then
+		vim.cmd("cprev") -- If we're at the last item, go to the previous item
+	else
+		vim.cmd("cfirst") -- Otherwise, go to the first item after removal
+	end
 end
 
 -- Create a command to call the function
-vim.api.nvim_create_user_command('RemoveQFItem', RemoveQFItem, {})
+vim.api.nvim_create_user_command("RemoveQFItem", RemoveQFItem, {})
 
 -- Autocommand for mapping 'dd' only in quickfix window
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'qf',
-  callback = function()
-    vim.api.nvim_buf_set_keymap(0, 'n', 'dd', ':RemoveQFItem<CR>', { noremap = true, silent = true })
-  end,
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "qf",
+	callback = function()
+		vim.api.nvim_buf_set_keymap(0, "n", "dd", ":RemoveQFItem<CR>", { noremap = true, silent = true })
+	end,
 })
 
 -- -- Auto-reindent and remove trailing whitespace on save
