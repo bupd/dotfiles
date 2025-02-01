888c734f268e9cb2615bd8f4723a2ca2738de631
bupd <bupdprasanth@gmail.com>
2025-02-01 17:49:41 +0530

    remove showing dash
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -77,14 +77,14 @@ autocmd("BufReadPost", {
 })
 
 -- Show Nvdash when all buffers are closed
-vim.api.nvim_create_autocmd("BufDelete", {
-	callback = function()
-		local bufs = vim.t.bufs
-		if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
-			vim.cmd("Nvdash")
-		end
-	end,
-})
+-- vim.api.nvim_create_autocmd("BufDelete", {
+-- 	callback = function()
+-- 		local bufs = vim.t.bufs
+-- 		if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+-- 			vim.cmd("Nvdash")
+-- 		end
+-- 	end,
+-- })
 
 -- -- Auto-reindent and remove trailing whitespace on save
 -- vim.api.nvim_create_autocmd("BufWritePre", {
