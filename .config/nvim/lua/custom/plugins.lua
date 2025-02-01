88855ab0f23d0bac4205f97c7b54b02c7bbfe8df
bupd <bupdprasanth@gmail.com>
2025-02-01 06:53:59 +0530

    add the commit msg here

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,5 +1,21 @@
 local plugins = {
 	{ "nvim-lua/plenary.nvim" },
+	{ "nvim-tree/nvim-web-devicons", lazy = true },
+	{
+		"nvchad/ui",
+		config = function()
+			require("nvchad")
+		end,
+	},
+	{
+		"nvchad/base46",
+		lazy = true,
+		build = function()
+			require("base46").load_all_highlights()
+		end,
+	},
+	-- "nvchad/volt", -- optional, needed for theme switcher
+	-- or just use Telescope themes
 	{ "ActivityWatch/aw-watcher-vim" },
 	{ "christoomey/vim-tmux-navigator", lazy = false },
 	{ "lewis6991/gitsigns.nvim", lazy = false },
