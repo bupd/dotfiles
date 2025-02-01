888ed49d76a7649e4b4abc9886136155b7b80e95
bupd <bupdprasanth@gmail.com>
2025-02-01 07:14:09 +0530

    update again working
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,12 @@
 require "core"
 
+-- put this in your main init.lua file ( before lazy setup )
+-- vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46_cache/"
+
+-- -- (method 2, for non lazyloaders) to load all highlights at once
+-- for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
+-- 	dofile(vim.g.base46_cache .. v)
+-- end
 local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
 if custom_init_path then
@@ -17,5 +24,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 
 dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
 vim.opt.rtp:prepend(lazypath)
 require "plugins"
+
+-- put this after lazy setup
+-- (method 1, For heavy lazyloaders)
+-- dofile(vim.g.base46_cache .. "defaults")
+-- dofile(vim.g.base46_cache .. "statusline")
--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -6,19 +6,6 @@ vim.cmd("set guicursor=n-v-c:block-Cursor-blinkwait1000-blinkon500-blinkoff300")
 -- turn off swap file
 vim.opt.swapfile = false
 
--- put this in your main init.lua file ( before lazy setup )
-vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46_cache/"
-
--- put this after lazy setup
-
--- (method 1, For heavy lazyloaders)
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
-
--- (method 2, for non lazyloaders) to load all highlights at once
-for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
-	dofile(vim.g.base46_cache .. v)
-end
 
 -- remove redundant trailing whitespace
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
