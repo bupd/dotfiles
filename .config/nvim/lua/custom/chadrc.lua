8881102c5080f457438500f427fe04bdfbb959ef
bupd <bupdprasanth@gmail.com>
2025-02-01 17:43:29 +0530

    add clear config for ui
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,6 +1,10 @@
 ---@type ChadrcConfig
 local M = {}
-M.ui = { theme = "kanagawa" }
+
+M.ui = { theme = "kanagawa", transparency = false, statusline = {
+	theme = "default",
+} }
 M.plugins = "custom.plugins"
 M.mappings = require("custom.mappings")
+
 return M
