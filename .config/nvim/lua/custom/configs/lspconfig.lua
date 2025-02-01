8889f0774b91f8ac8a9abdab07b3bf40b0fbf80d
bupd <bupdprasanth@gmail.com>
2025-02-01 05:56:50 +0530

    update lua lsp config
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -3,6 +3,7 @@ local on_attach = configs.on_attach
 local capabilities = configs.capabilities
 
 local lspconfig = require("lspconfig")
+
 local servers = {
 	"html",
 	"clangd",
@@ -66,6 +67,39 @@ for _, lsp in ipairs(servers) do
 						["/path/from/root/of/project"] = "/.github/workflows/*",
 					},
 				},
+			},
+		})
+	elseif lsp == "lua_ls" then
+		lspconfig[lsp].setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+			on_init = function(client)
+				local path = client.workspace_folders[1].name
+				if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+					return
+				end
+				client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+					runtime = {
+						-- Tell the language server which version of Lua you're using
+						-- (most likely LuaJIT in the case of Neovim)
+						version = "LuaJIT",
+					},
+					-- Make the server aware of Neovim runtime files
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME,
+							-- Depending on the usage, you might want to add additional paths here.
+							-- "${3rd}/luv/library"
+							-- "${3rd}/busted/library",
+						},
+						-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+						-- library = vim.api.nvim_get_runtime_file("", true)
+					},
+				})
+			end,
+			settings = {
+				Lua = {},
 			},
 		})
 	else
