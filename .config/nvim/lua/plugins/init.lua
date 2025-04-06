f299a403d7c2591958d2c63f3596aa3e53a94d43
bupd <bupdprasanth@gmail.com>
2025-04-06 14:24:02 +0530

    update plugins
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -12,6 +12,132 @@ return {
       require "configs.lspconfig"
     end,
   },
+  { "christoomey/vim-tmux-navigator", lazy = false },
+  { "lewis6991/gitsigns.nvim",        lazy = false },
+  {
+    "mbbill/undotree",
+    lazy = false,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "javascript",
+      "typescript",
+      "javascriptreact",
+      "typescriptreact",
+      "html",
+    },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  -- astrojs plugin
+  { "wuelnerdotexe/vim-astro" },
+
+  -- mason ensure_installed
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "angular-language-server@16.2.0",
+        "typescript-language-server",
+        "tailwindcss-language-server",
+        "astro-language-server",
+        "bash-language-server",
+        "lua-language-server",
+        "dockerfile-language-server",
+        "docker-compose-language-service",
+        "gopls",
+        "goimports",
+        "stylua",
+        "isort",
+        "yamlfix",
+        "yaml-language-server",
+        "prettierd",
+        "gofumpt",
+        "golines",
+        "gospel",
+        "grammarly-languageserver",
+        "eslint-lsp",
+        "pyright",
+        "mypy",
+        "ruff",
+        "black",
+        "debugpy",
+      },
+    },
+  },
+  -- { "williamboman/mason-lspconfig.nvim" },
+  -- nvim notify
+  { "rcarriga/nvim-notify",   lazy = false },
+  -- gopher for new things
+  {
+    "olexsmir/gopher.nvim",
+    requires = { -- dependencies
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd([[silent! GoInstallDeps]])
+    end,
+  },
+  -- harpoon
+  {
+    "ThePrimeagen/harpoon",
+    event = "VeryLazy",
+    config = function(_)
+      require("configs.harpoon")
+    end,
+  },
+  { "wakatime/vim-wakatime", lazy = false },
+  -- vim fugitive
+  {
+    "tpope/vim-fugitive",
+    cmd = { "Git", "G" },
+  },
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>tx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>tX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>tcs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>tcl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>txL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>txQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  },
+
 
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
