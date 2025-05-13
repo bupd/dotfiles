8889053fd821abc8ea4a18d4340fed3f860ddb3d
bupd <bupdprasanth@gmail.com>
2025-05-14 05:02:13 +0530

    update nvchad
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -6,19 +6,20 @@
 local M = {}
 
 M.base46 = {
-	theme = "kanagawa",
+  theme = "kanagawa",
+  transparency = true,
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+M.nvdash = { load_on_startup = true }
+M.ui = {
+      tabufline = {
+         lazyload = false
+     }
+}
 
 return M
