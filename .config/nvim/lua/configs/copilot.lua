888dc2daa9ff9fd3ceb673cb202866660a42b49f
bupd <bupdprasanth@gmail.com>
2025-06-27 04:21:49 +0530

    update copilot
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/configs/copilot.lua
+++ .config/nvim/lua/configs/copilot.lua
@@ -20,7 +20,7 @@ require('copilot').setup({
     auto_trigger = true,
     hide_during_completion = true,
     debounce = 75,
-    trigger_on_accept = true,
+    trigger_on_accept = false,
     keymap = {
       accept = "<C-[>",
       accept_word = false,
@@ -42,36 +42,36 @@ require('copilot').setup({
     ["."] = false,
   },
   auth_provider_url = nil, -- URL to authentication provider, if not "https://github.com/"
-  logger = {
-    file = vim.fn.stdpath("log") .. "/copilot-lua.log",
-    file_log_level = vim.log.levels.OFF,
-    print_log_level = vim.log.levels.WARN,
-    trace_lsp = "off", -- "off" | "messages" | "verbose"
-    trace_lsp_progress = false,
-    log_lsp_messages = false,
-  },
+  -- logger = {
+  --   file = vim.fn.stdpath("log") .. "/copilot-lua.log",
+  --   file_log_level = vim.log.levels.OFF,
+  --   print_log_level = vim.log.levels.WARN,
+  --   trace_lsp = "off", -- "off" | "messages" | "verbose"
+  --   trace_lsp_progress = false,
+  --   log_lsp_messages = false,
+  -- },
   copilot_node_command = 'node', -- Node.js version must be > 20
   workspace_folders = {},
   copilot_model = "",
   root_dir = function()
     return vim.fs.dirname(vim.fs.find(".git", { upward = true })[1])
   end,
-  should_attach = function(_, _)
-    if not vim.bo.buflisted then
-      logger.debug("not attaching, buffer is not 'buflisted'")
-      return false
-    end
-
-    if vim.bo.buftype ~= "" then
-      logger.debug("not attaching, buffer 'buftype' is " .. vim.bo.buftype)
-      return false
-    end
-
-    return true
-  end,
-  server = {
-    type = "nodejs", -- "nodejs" | "binary"
-    custom_server_filepath = nil,
-  },
+  -- should_attach = function(_, _)
+  --   if not vim.bo.buflisted then
+  --     logger.debug("not attaching, buffer is not 'buflisted'")
+  --     return false
+  --   end
+  --
+  --   if vim.bo.buftype ~= "" then
+  --     logger.debug("not attaching, buffer 'buftype' is " .. vim.bo.buftype)
+  --     return false
+  --   end
+  --
+  --   return true
+  -- end,
+  -- server = {
+  --   type = "nodejs", -- "nodejs" | "binary"
+  --   custom_server_filepath = nil,
+  -- },
   server_opts_overrides = {},
 })
