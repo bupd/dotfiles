8882b00d142eec0745bd95be17ea39c1f40d1f7f
bupd <bupdprasanth@gmail.com>
2025-06-27 19:24:58 +0530

    fix: supermaven with correct keymaps
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/configs/supermaven.lua
+++ .config/nvim/lua/configs/supermaven.lua
@@ -1,23 +1,14 @@
 require("supermaven-nvim").setup {
   keymaps = {
-    accept_suggestion = "<C-]>", -- handled by nvim-cmp / blink.cmp
+    accept_suggestion = "<Right>",
     clear_suggestion = "<C-c>",
-    -- accept_word = "<C-]>",
+    accept_word = "<C-]>",
   },
   -- ignore_filetypes = { cpp = true }, -- or { "cpp", }
-  ignore_filetypes = { "cpp" }, -- or { "cpp", }
+  ignore_filetypes = { "cpp", "bigfile", "snacks_input", "snacks_notif" }, -- or { "cpp", }
   color = {
     suggestion_color = "#ffffff",
     cterm = 244,
-  },
-  opts = {
-    keymaps = {
-      accept_suggestion = "<C-]>", -- handled by nvim-cmp / blink.cmp
-      clear_suggestion = "<C-c>",
-      -- accept_word = "<C-]>",
-    },
-    -- disable_inline_completion = true,
-    ignore_filetypes = { "bigfile", "snacks_input", "snacks_notif" },
   },
   log_level = "info", -- set to "off" to disable logging completely
   disable_inline_completion = false, -- disables inline completion for use with cmp
