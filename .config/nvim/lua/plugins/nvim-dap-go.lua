888ffe4517b066a00744bf132882229d626374ee
bupd <bupdprasanth@gmail.com>
2025-09-13 05:55:38 +0530

    feat: update nvim dap plugin
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/plugins/nvim-dap-go.lua
+++ .config/nvim/lua/plugins/nvim-dap-go.lua
@@ -1,10 +1,16 @@
 return {
   "leoluz/nvim-dap-go",
   ft = { "go" },
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "mfussenegger/nvim-dap",
+    "theHamsta/nvim-dap-virtual-text",
+    -- "nvim-neotest/nvim-nio",
+  },
   config = function()
     require("dap-go").setup()
   end,
-  -- opts = function()
-  --   return require "myconfig"
-  -- end,
+  opts = function()
+    return require "myconfig"
+  end,
 }
