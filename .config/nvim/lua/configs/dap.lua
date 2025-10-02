888412678e538ad580157ba7a43def5ab47163ac
bupd <bupdprasanth@gmail.com>
2025-10-02 21:24:23 +0530

    feat: workable config
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/configs/dap.lua
+++ .config/nvim/lua/configs/dap.lua
@@ -1,71 +1,111 @@
-local dap_go = require "dap-go"
-local dap = require "dap"
-local dap_ui = require "dapui"
+local dap_go = require("dap-go")
+local dap = require("dap")
+local dap_ui = require("dapui")
 
+-- Ensure delve is installed
 require("mason-nvim-dap").setup({
-    ensure_installed = { "delve" }
+  ensure_installed = { "delve" },
 })
 
+-- dap-go sets up normal local debugging for you
 dap_go.setup()
--- For One
+
+-- Remote debugging: One container
 table.insert(dap.configurations.go, {
-  type = "go",
+  type = "go",                -- uses dap-go adapter
   name = "One CONTAINER debugging",
-  host = "localhost",
-  port = 4001,
   mode = "remote",
   request = "attach",
+  host = "127.0.0.1",          -- or container IP if not port-forwarded
+  port = 4001,                 -- must match dlv server
   substitutePath = {
-    -- only for mac pottais
-    -- { from = '/opt/homebrew/Cellar/go/1.23.1/libexec', to = '/usr/local/go'},
-    { from = "${workspaceFolder}", to = "/home/root/go/bin/dlv" },
+    { from = "/app", to = "${workspaceFolder}" }, -- container path -> local
   },
 })
 
--- -- For Two
--- table.insert(dap.configurations.go, {
---   type = "delvetwo",
---   name = "Two CONTAINER debugging",
---   mode = "remote",
---   request = "attach",
---   substitutePath = {
---     -- { from = "/opt/homebrew/Cellar/go/1.23.1/libexec", to = "/usr/local/go" },
---     { from = "${workspaceFolder}", to = "dlv" },
---   },
--- })
-
--- adapters configuration
-dap.adapters.go = {
-  type = "server",
-  host = "localhost",
-  port = "4001",
-}
-
--- dap.adapters.delvetwo = {
---   type = "server",
---   host = "127.0.0.1",
---   port = "2346",
--- }
-
+-- dap-ui setup
 dap_ui.setup {
   layouts = {
     {
       elements = {
-        {
-          id = "scopes",
-          size = 0.35,
-        },
-        {
-          id = "breakpoints",
-          size = 0.30,
-        },
-        {
-          id = "repl",
-          size = 0.35,
-        },
+        { id = "scopes", size = 0.35 },
+        { id = "breakpoints", size = 0.30 },
+        { id = "repl", size = 0.35 },
       },
       position = "right",
       size = 50,
     },
   },
 }
+
+-- local dap_go = require "dap-go"
+-- local dap = require "dap"
+-- local dap_ui = require "dapui"
+--
+-- require("mason-nvim-dap").setup({
+--     ensure_installed = { "delve" }
+-- })
+--
+-- dap_go.setup()
+-- -- For One
+-- table.insert(dap.configurations.go, {
+--   type = "go",
+--   name = "One CONTAINER debugging",
+--   host = "localhost",
+--   port = 4001,
+--   mode = "remote",
+--   request = "attach",
+--   substitutePath = {
+--     -- only for mac pottais
+--     -- { from = '/opt/homebrew/Cellar/go/1.23.1/libexec', to = '/usr/local/go'},
+--     { from = "${workspaceFolder}", to = "/home/root/go/bin/dlv" },
+--   },
+-- })
+--
+-- -- -- For Two
+-- -- table.insert(dap.configurations.go, {
+-- --   type = "delvetwo",
+-- --   name = "Two CONTAINER debugging",
+-- --   mode = "remote",
+-- --   request = "attach",
+-- --   substitutePath = {
+-- --     -- { from = "/opt/homebrew/Cellar/go/1.23.1/libexec", to = "/usr/local/go" },
+-- --     { from = "${workspaceFolder}", to = "dlv" },
+-- --   },
+-- -- })
+--
+-- -- adapters configuration
+-- dap.adapters.go = {
+--   type = "server",
+--   host = "localhost",
+--   port = "4001",
+-- }
+--
+-- -- dap.adapters.delvetwo = {
+-- --   type = "server",
+-- --   host = "127.0.0.1",
+-- --   port = "2346",
+-- -- }
+--
+-- dap_ui.setup {
+--   layouts = {
+--     {
+--       elements = {
+--         {
+--           id = "scopes",
+--           size = 0.35,
+--         },
+--         {
+--           id = "breakpoints",
+--           size = 0.30,
+--         },
+--         {
+--           id = "repl",
+--           size = 0.35,
+--         },
+--       },
+--       position = "right",
+--       size = 50,
+--     },
+--   },
+-- }
