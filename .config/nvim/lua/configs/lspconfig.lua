8889fa0bce37f55cf7268af331a265505b17dddf
bupd <bupdprasanth@gmail.com>
2025-10-02 21:01:20 +0530

    feat: improved lspconfig
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,8 +1,6 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
 -- EXAMPLE
 local servers = {
   -- "pylsp",
@@ -16,17 +14,20 @@ local servers = {
 	"docker_compose_language_service",
 	"dockerls",
 }
+vim.lsp.enable(servers)
 
-local nvlsp = require "nvchad.configs.lspconfig"
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
+-- old config
+--
+-- local nvlsp = require "nvchad.configs.lspconfig"
+--
+-- -- lsps with default config
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     on_attach = nvlsp.on_attach,
+--     on_init = nvlsp.on_init,
+--     capabilities = nvlsp.capabilities,
+--   }
+-- end
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
